5f97d973294244065d51d4ba04c10574567a4c33
clare0125 <clare.woozie1@gmail.com>
2018-07-13 18:45:56 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,17 @@
 class Person
   
   def name
-    @name
+    name = @name
   end
+  
+  def name=(n_name)
+    @name = name
+  end
+  
+  def job
+    job = @job
+  end
+  
+  def job=(n_job)
+    @job = job
 end
